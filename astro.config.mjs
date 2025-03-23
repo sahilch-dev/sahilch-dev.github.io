de5325612d9070e659f895acb149fda734be8ee5
Sahil Chourasiya <sahil.chourasiya160@gmail.com>
2025-03-23 12:26:12 +0530

    astro page deploy

---
+++ astro.config.mjs
@@ -0,0 +1,8 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+
+// https://astro.build/config
+export default defineConfig({
+    site: 'https://sahich-dev.github.io',
+    base: 'my-repo',
+});
