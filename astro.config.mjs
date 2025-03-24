b64a26ef50a2a86f57c84ddf010f56ed45c0a901
Sahil Chourasiya <sahil.chourasiya160@gmail.com>
2025-03-24 15:57:03 +0530

    tailwind  and sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,18 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import tailwindcss from '@tailwindcss/vite';
+
+import sitemap from '@astrojs/sitemap';
+
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://sahich-dev.github.io',
-    base: 'my-repo',
+  site: 'https://sahich-dev.github.io',
+  base: '.',
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
+
+  integrations: [sitemap()],
 });
